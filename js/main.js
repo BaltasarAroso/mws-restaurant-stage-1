9cc9ca50e00667394177fa6dc5c673b1a9f7057a
BaltasarAroso <ba_aroso@icloud.com>
2018-04-10 00:28:42 +0100

    starting focus

--- js/main.js
+++ js/main.js
@@ -144,8 +144,9 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.photograph_alt;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
+  name.tabindex = 0;
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -159,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
   return li
 }
