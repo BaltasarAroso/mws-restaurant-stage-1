882f6f1cc1a54bf635e5cad277ac36e700cf0c52
BaltasarAroso <ba_aroso@icloud.com>
2018-07-20 14:44:59 +0100

    1) added gulp features 2) added browserSync 3) implementation of gulp 4 notation command to run: 'gulp' and 'gulp clean' before commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,87 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () {
-  // place code for your default task here
-  console.log('Hello Gulp! gulpfile.js test...');
-  // done();
-});
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    postcss       = require('gulp-postcss'),
+    cssnano       = require('cssnano'),
+    autoprefixer  = require('autoprefixer'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    del           = require('del'),
+    browserSync   = require('browser-sync').create();
 
-gulp.task('styles', function(done) {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./css'));
-  done();
-});
+var build = gulp.series(clean, gulp.parallel(styles, css, html, js, watch));
+
+var paths = {
+  styles: {
+    src: 'sass/*.scss',
+    dest: 'css'
+  },
+  css: {
+    src: 'css/*.css',
+    dest: 'assets/css'
+  },
+  html: {
+    src: '*.html',
+    dest: 'assets/'
+  },
+  js: {
+    src: 'js/*.js',
+    dest: 'assets/js'
+  }
+}
+
+function clean() {
+  del(['assets'])
+  return del(['css/*'])
+}
+
+function reload() {
+  browserSync.reload()
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass()).on('error', sass.logError)
+            .pipe(postcss([
+              autoprefixer({
+                browsers: ['last 2 versions']
+              }),
+              cssnano()
+            ]))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+}
+
+function css() {
+  return gulp.src(paths.css.src)
+            .pipe(gulp.dest(paths.css.dest))
+}
+
+function html() {
+  return gulp.src(paths.html.src)
+            .pipe(gulp.dest(paths.html.dest))
+}
+
+function js() {
+  return gulp.src(paths.js.src)
+            .pipe(gulp.dest(paths.js.dest))
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  })
+  gulp.watch(paths.styles.src, reload)
+  gulp.watch(paths.css.src, reload)
+  gulp.watch(paths.html.src, reload)
+  gulp.watch(paths.js.src, reload)
+}
+
+exports.styles = styles
+exports.watch = watch
+exports.clean = clean
+
+gulp.task('default', build)
