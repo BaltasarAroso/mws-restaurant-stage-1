faacde21f16c5d65b873d455ca2168d9cebb3644
BaltasarAroso <ba_aroso@icloud.com>
2018-08-06 01:31:50 +0100

    code cleaned

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -15,29 +15,7 @@ class DBHelper {
 		return `http://localhost:${port}/restaurants`;
 	}
 
-	/**
-	 * Fetch all restaurants.
-	 */
-	// static fetchRestaurants(callback) {
-	// 	let xhr = new XMLHttpRequest();
-	// 	xhr.open('GET', DBHelper.DATABASE_URL);
-	// 	xhr.onload = () => {
-	// 		if (xhr.status === 200) {
-	// 			// Got a success response from server!
-	// 			const json = JSON.parse(xhr.responseText);
-	// 			const restaurants = json.restaurants;
-	// 			callback(null, restaurants);
-	// 		} else {
-	// 			// Oops!. Got an error from server.
-	// 			const error = `Request failed. Returned status of ${xhr.status}`;
-	// 			callback(error, null);
-	// 		}
-	// 	};
-	// 	xhr.send();
-	// }
-
 	//============================ Configure IndexedDB functions ============================//
-
 	/**
 	 * Open IndexedDB
 	 */
@@ -64,82 +42,40 @@ class DBHelper {
 	/**
 	 * Read From IndexedDB
 	 */
-	static ReadFromDB(dbPromise, objName, key) {
-		return dbPromise.then(function(db) {
-			var tx = db.transaction(objName);
-			var objStore = tx.objectStore(objName);
-			return objStore.get(key);
-		});
-		// .then(function(val) {
-		// 	console.log('The value of ', key, ' is:', val);
-		// });
-	}
-
-	static ReadAllFromDB(dbPromise, objName) {
-		return dbPromise.then(function(db) {
-			var tx = db.transaction(objName);
-			var objStore = tx.objectStore(objName);
+	static ReadAllFromDB(dbElementName) {
+		return DBHelper.OpenDB('restaurant-review-DB').then(function(db) {
+			var tx = db.transaction(dbElementName);
+			var objStore = tx.objectStore(dbElementName);
 			return objStore.getAll();
 		});
-		// .then(function(val) {
-		// 	console.log(objName, ': ', val);
-		// });
-	}
-
-	static ReadAllInOrderFromDB(dbPromise, objName, order) {
-		return dbPromise
-			.then(function(db) {
-				var tx = db.transaction(objName);
-				var objStore = tx.objectStore(objName);
-				var objIndex = objStore.index(order);
-				return objIndex.getAll();
-			})
-			.then(function(val) {
-				console.log(objName, 'by ', order, ': ', val);
-			});
 	}
 
 	/**
 	 * Write/Save to IndexedDB
 	 */
-	static WriteKeyValueToDB(dbPromise, objName, key2write, value2write) {
-		return dbPromise.then(function(db) {
-			var tx = db.transaction(objName, 'readwrite');
-			var objStore = tx.objectStore(objName);
-			objStore.put(key2write, value2write);
-			return tx.complete;
-		});
-		// .then(function() {
-		// 	console.log('Added ', key2write, ':', value2write, 'to ', objName);
-		// });
-	}
-
-	static WriteToDB(dbPromise, data, objName, tag) {
-		return dbPromise.then(function(db) {
-			var tx = db.transaction(objName, 'readwrite');
-			var objStore = tx.objectStore(objName);
+	static WriteToDB(data, dbElementName, tag) {
+		return DBHelper.OpenDB('restaurant-review-DB').then(function(db) {
+			var tx = db.transaction(dbElementName, 'readwrite');
+			var objStore = tx.objectStore(dbElementName);
 			// tag = 1 means 'restaurants' objStore and tag = 0 means 'reviews' objStore
 			tag ? Array.from(data).forEach(item => objStore.put(item)) : objStore.put(data);
 			return tx.complete;
 		});
-		// .then(function() {
-		// 	console.log('Added ', obj2write, 'to ', objName);
-		// });
 	}
 
 	/**
 	 * Get Data From URL
 	 */
-	static GetDataFromURL(dbPromise, tag) {
+	static GetDataFromURL(tag) {
 		return fetch(DBHelper.DATABASE_URL)
 			.then(response => {
 				return response.json();
 			})
 			.then(data => {
 				if (tag) {
-					DBHelper.WriteToDB(dbPromise, data, 'restaurants', 1);
+					DBHelper.WriteToDB(data, 'restaurants', RESTAURANT);
 				} else {
-					DBHelper.WriteToDB(dbPromise, data, `reviews-${self.restaurant.id}`, 0);
+					DBHelper.WriteToDB(data, `reviews-${self.restaurant.id}`, REVIEW);
 					console.log(`Reviews data from API for restaurant: ${self.restaurant.id}`);
 					console.log(data);
 				}
@@ -147,91 +83,23 @@ class DBHelper {
 				return data;
 			});
 	}
+	//========================================================================================//
 
-	//============================ Using Fetch API ============================//
-
+	/**
+	 * Fetch all restaurants.
+	 */
 	static fetchRestaurants(callback) {
-		var dbPromise = DBHelper.OpenDB('restaurant-review-DB');
-		DBHelper.ReadAllFromDB(dbPromise, 'restaurants')
+		DBHelper.ReadAllFromDB('restaurants')
 			.then(data => {
 				if (data.length == 0) {
-					return DBHelper.GetDataFromURL(dbPromise, RESTAURANT);
+					return DBHelper.GetDataFromURL(RESTAURANT);
 				}
 				return data;
 			})
 			.then(restaurants => {
 				callback(null, restaurants);
 			});
-		// DBHelper.WriteKeyValueToDB(dbPromise, 'keyval', 'bar', 'foo');
-		// DBHelper.WriteToDB(dbPromise, 'people', { name: 'John', age: '21', favouriteAnimal: 'dog' });
-		// DBHelper.WriteToDB(dbPromise, 'people', { name: 'Helton', age: '34', favouriteAnimal: 'cat' });
-		// DBHelper.WriteToDB(dbPromise, 'people', { name: 'Peter', age: '15', favouriteAnimal: 'cat' });
-		// DBHelper.ReadFromDB(dbPromise, 'keyval', 'hello');
-		// DBHelper.ReadAllFromDB(dbPromise, 'people');
-		// DBHelper.ReadAllInOrderFromDB(dbPromise, 'restaurants', 'reviews');
 	}
-
-	//	============================== With LocalStorage ==============================//
-
-	// /**
-	//  * Save Restaurants
-	//  */
-	// static saveRestaurants(restaurants) {
-	// 	localStorage.setItem('restaurants', restaurants);
-	// }
-
-	// /**
-	//  * Get Restaurants from Local Storage
-	//  */
-	// static getRestaurantsFromLocalStorage(callback) {
-	// 	return localStorage.getItem('restaurants');
-	// }
-
-	// /**
-	//  * Get Restaurants remotelly from the server (DATABASE_URL)
-	//  */
-	// static getRestaurantsFromServer(callback = () => null) {
-	// 	fetch(DBHelper.DATABASE_URL)
-	// 		.then(response => {
-	// 			return response.json();
-	// 		})
-	// 		.then(data => {
-	// 			DBHelper.saveRestaurants(data);
-	// 			callback(null, data);
-	// 		})
-	// 		.catch(err => {
-	// 			const error = `Request failed. Returned status of ${err}`;
-	// 			callback(error, null);
-	// 		});
-	// }
-
-	// /**
-	//  * Fetch all restaurants.
-	//  */
-	// static fetchRestaurants(callback) {
-	// 	fetch(DBHelper.DATABASE_URL)
-	// 		.then(response => {
-	// 			return response.json();
-	// 		})
-	// 		.then(restaurants => {
-	// 			DBHelper.saveRestaurants(restaurants);
-	// 			callback(null, restaurants);
-	// 		})
-	// 		.catch(err => {
-	// 			const error = `Connection to server failed. Returned status of ${err}`;
-	// 			callback(error, null);
-	// 			return DBHelper.getRestaurants()
-	// 				.then(response => {
-	// 					return callback(null, response);
-	// 				})
-	// 				.catch(err => {
-	// 					const error = `Request failed. Returned status of ${err}`;
-	// 					callback(error, null);
-	// 				});
-	// 		});
-	// }
-
-	//=======================================================================================//
 
 	/**
 	 * Fetch a restaurant by its ID.
@@ -378,16 +246,16 @@ class DBHelper {
 		return marker;
 	}
 
-	//========================== Added to fetch reviews by the IDB ========================== //
+	//========================== Added to fetch reviews according to IDB ========================== //
+
 	/**
-	 * Fetch all restaurant reviws by its ID.
+	 * Fetch all restaurant reviews by its ID.
 	 */
 	static fetchReviewsById(id, callback) {
-		var dbPromise = DBHelper.OpenDB('restaurant-review-DB');
-		DBHelper.ReadAllFromDB(dbPromise, `reviews-${id}`)
+		DBHelper.ReadAllFromDB(`reviews-${id}`)
 			.then(data => {
 				if (data.length == 0) {
-					return DBHelper.GetDataFromURL(dbPromise, REVIEW);
+					return DBHelper.GetDataFromURL(REVIEW);
 				}
 				return data;
 			})
